7204996bf2fbcf2dc55fc91c1735d20a4e32141c
suecharo <suehiro619@gmail.com>
2020-05-03 13:29:17 +0900

    Fix mypy ignore line

--- stayhome_wes/type.py
+++ stayhome_wes/type.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, List
 if version_info.minor < 8:
     from typing_extensions import TypedDict
 else:
-    from typing import TypedDict  # type: ignore
+    from typing import TypedDict  # mypy: ignore
 
 
 class DefaultWorkflowEngineParameter(TypedDict):
