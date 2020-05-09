d71525f9aeff4b01fd81ab73e7ffdf623059daca
suecharo <suehiro619@gmail.com>
2020-05-09 20:49:42 +0900

    Fix default run dir path

--- genpei/const.py
+++ genpei/const.py
@@ -3,13 +3,20 @@
 from pathlib import Path
 from typing import Dict
 
+
+SRC_DIR: Path = Path(__file__).parent.resolve()
+
+SERVICE_INFO_JSON: Path = \
+    SRC_DIR.joinpath("service_info.json").resolve()
+DEFAULT_RUN_DIR = Path.cwd().parent.joinpath("run").resolve()
 DEFAULT_HOST: str = "127.0.0.1"
 DEFAULT_PORT: int = 8080
 GET_STATUS_CODE: int = 200
 POST_STATUS_CODE: int = 200
+DATE_FORMAT: str = "%Y-%m-%dT%H:%M:%S"
+CANCEL_TIMEOUT: int = 10
 
 RUN_DIR_STRUCTURE: Dict[str, str] = {
-    "service_info": "service-info.json",
     "run_request": "run_request.json",
     "state": "state.txt",
     "exe": "exe",
@@ -22,14 +29,6 @@ RUN_DIR_STRUCTURE: Dict[str, str] = {
     "stderr": "stderr.log",
     "pid": "run.pid",
     "cmd": "cmd.txt",
-    "sys_error": "sys_error.log"
+    "sys_error": "sys_error.log",
+    "tasks": "tasks"
 }
-
-DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
-
-CANCEL_TIMEOUT = 10
-
-SRC_DIR: Path = Path(__file__).parent.resolve()
-SERVICE_INFO_JSON: Path = \
-    SRC_DIR.joinpath(RUN_DIR_STRUCTURE["service_info"]).resolve()
-DEFAULT_RUN_DIR = SRC_DIR.parent.joinpath("run").resolve()
